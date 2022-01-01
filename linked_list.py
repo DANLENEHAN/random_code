316043ab3522dd4e3b18c253b18ca02035c0662b
Dan Lenehan <danlenehan@LAPTOP-5MQN8F9R.localdomain>
2022-01-01 14:21:06 +0000

    Circular LL good imp

--- linked_list.py
+++ linked_list.py
@@ -8,17 +8,18 @@ def ll_is_circular(head):
     Is a Linked List circular
     """
 
-    curr_node = head.next
+    nodes = set()
+    curr_node = head
 
     # Iterating through LL
     while (
-        curr_node.next is not None and
-        curr_node != head
+        curr_node is not None and
+        curr_node not in nodes
     ):
+        nodes.add(curr_node)
         curr_node = curr_node.next
 
-    return curr_node == head
-
+    return curr_node in nodes
 
 
 class Node:
@@ -39,10 +40,23 @@ if __name__ == "__main__":
     for number in range(1, 10):
         next_node = Node(number)
         curr_node.next = next_node
+        if number %5 == 0:
+            keep_node = curr_node
         curr_node = next_node
 
     # Make LL circular
-    curr_node.next = head_node
+    curr_node.next = keep_node
+
+    print("Printing List:")
+    node = head_node
+    count = 1
+    while (
+        node.next and 
+        count < 16
+    ):
+        print(f"curr node Data: {node.data}, ID: {id(node)}")
+        node = node.next
+        count += 1
 
     print(
         "Linked is circular: "
